3c0311f4fc6586d4642faf2347a4500cc9650c5f
Rinorragi <14329249+Rinorragi@users.noreply.github.com>
2025-02-14 09:32:13 +0200

    Lab41: Run TLS scanning against docker target

--- scripts/tlsaudit.ps1
+++ scripts/tlsaudit.ps1
@@ -1,9 +1,11 @@
 param(
   [Parameter(Mandatory = $true)]
-  [string]$domain
+  [string]$domain,
+  [Parameter(Mandatory = $true)]
+  [string]$port
 )
 
-$nmapResult = nmap -sV --script ssl-enum-ciphers -p 443 $domain
+$nmapResult = nmap -sV --script ssl-enum-ciphers -p $port $domain
 $ciphers = $nmapResult | Select-String "TLS_" | ForEach-Object { $_[0].Line.Substring(8) }
 $gradeRow = ($nmapResult | Select-String "least strength:")[0][0].Line
 $grade = $gradeRow.Substring($gradeRow.Length - 1, 1)
