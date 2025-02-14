3c7b8e2d3017a67287a84e1e6be7f700e56e73d3
Rinorragi <14329249+Rinorragi@users.noreply.github.com>
2025-02-14 09:32:22 +0200

    Lab-51: Fix file-list commithook

--- scripts/githooks/pre-commit-file-list.ps1
+++ scripts/githooks/pre-commit-file-list.ps1
@@ -1,9 +1,14 @@
 [CmdletBinding()]
 param()
 
-$fileList = git ls-files --others --exclude-standard --modified
+# Staged files that are about to be committed
+$fileList = git diff --cached --name-only
 
 Write-Host "These are the files that you have modified or are about to add:" -ForegroundColor Green
+Write-Host "---------------------------------------------------------------"
 $fileList | ForEach-Object { Write-Host $_ -ForegroundColor Green }
+Write-Host "---------------------------------------------------------------"
+Write-Host "You could do some analysis here before commit happens"
 
-exit 1
+exit 0 # Commit succeed
+# exit 1 # Commit would fail
